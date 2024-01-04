5a0ea0a8083a8d4e77065e8c3f4a38e1bfaad975
vivek <vivek.kachhadiya23@gmail.com>
2024-01-04 20:38:33 +0530

    fix: pigeon shipment API

--- pigeon-shipments/src/services/shipment.ts
+++ pigeon-shipments/src/services/shipment.ts
@@ -1,13 +1,26 @@
+import shipmentsData from "../mocks/shipments.json";
+
 export const loadShipments = async () => {
   if (sessionStorage.getItem("shipmentsData")) {
     return JSON.parse(sessionStorage.getItem("shipmentsData") ?? "[]");
   }
 
-  const shipmentsRequest = await fetch(
-    "https://raw.githubusercontent.com/vivek-kachhadiya13/microfrontend-vue-and-vite/main/pigeon-shipments/src/mocks/shipments.json"
-  );
+  // const shipmentsRequest = await fetch(
+  //   "https://raw.githubusercontent.com/vivek-kachhadiya13/microfrontend-vue-and-vite/main/pigeon-shipments/src/mocks/shipments.json",
+  //   {
+  //     headers: {
+  //       Accept: "application/json",
+  //       "Content-Type": "application/json",
+  //       // Authorization: `Basic ${Buffer.from(
+  //       //   `${import.meta.env.VITE_GITHUB_USERNAME}:
+  //       //   ${import.meta.env.VITE_GITHUB_PERSONAL_TOKEN}`
+  //       // ).toString("base64")}`,
+  //       authorization: `Basic ${import.meta.env.VITE_GITHUB_API_TOKEN}`,
+  //     },
+  //   }
+  // );
 
-  const shipmentsData = await shipmentsRequest.json();
+  // const shipmentsData = await shipmentsRequest.json();
 
   sessionStorage.setItem("shipmentsData", JSON.stringify(shipmentsData));
 
